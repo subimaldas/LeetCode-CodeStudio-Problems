109f7c4946d0ac24ceb6ce04ee49434cedf07d79
Subimal Das <er.subimaldas@gmail.com>
2022-07-09 17:08:04 +0530

    Find Unique

---
+++ Arrays/FindUnique.cpp
@@ -0,0 +1,15 @@
+Find unique
+
+Approach: Take a variable ans and **XOR** it with every Element of array.
+
+
+CODE:
+int findUnique(int *arr, int size)
+{
+    int ans=0;
+    for(int i=0;i<size;i++){
+        //Using XOR
+        ans=ans^arr[i];
+    }
+    return ans;
+}
