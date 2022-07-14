8f0d9ae1688d4ea7ca84c8023a78a6933fafe3c6
Subimal Das <er.subimaldas@gmail.com>
2022-07-14 09:37:48 +0530

    10. Peak Element in a mountain array.
    https://leetcode.com/problems/peak-index-in-a-mountain-array/

--- Peak Index in a Mountain Array.cpp
+++ Peak Index in a Mountain Array.cpp
@@ -1,4 +1,17 @@
-class Solution {
+Question: Find the peak element in a mountain array
+
+Approach: 
+1. if arr[mid]< arr[mid=1] move start,  
+2. else end=mid, here we can't use mid-1 because it will move the element before the peak element.
+    
+    
+    Time Complexity: O(Logn). 
+    Auxiliary Space: O(log n). 
+ 
+        
+CODE:
+        
+        class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int   s = 0;
