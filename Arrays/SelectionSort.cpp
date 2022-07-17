59d4bce0771877386394c83d12b5f8ada1fc7cd6
Subimal Das <er.subimaldas@gmail.com>
2022-07-17 22:49:38 +0530

    Create SelectionSort.cpp

---
+++ Arrays/SelectionSort.cpp
@@ -0,0 +1,11 @@
+Approach:
+
+1. In different round, we take the minimum value and  place it in the 1st place and following the same.
+
+2.  Swap the are[i],minIndex
+
+3. Round = n-1
+
+Time Complexity: O(n²)
+
+Space Complexity:O(1)
