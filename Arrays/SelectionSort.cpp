bffb81fee0646dcbbf566d4155ed51481cb3c36e
Subimal Das <er.subimaldas@gmail.com>
2022-07-18 08:51:54 +0530

    Selection Sort
    https://www.codingninjas.com/codestudio/problems/selection-sort_981162

--- Arrays/SelectionSort.cpp
+++ Arrays/SelectionSort.cpp
@@ -1,11 +1,29 @@
 Approach:
 
 1. In different round, we take the minimum value and  place it in the 1st place and following the same.
-
 2.  Swap the are[i],minIndex
-
 3. Round = n-1
 
 Time Complexity: O(n²)
-
 Space Complexity:O(1)
+  
+  
+CODE:
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i = 0; i < n-1; i++ ) {
+        // We  consider the 1st element as the minValue
+        int minIndex = i;
+        
+        for(int j = i+1; j<n; j++) {
+            
+            //if we get any element greater then the 1st element swap them, thus updating the j
+            if(arr[j] < arr[minIndex]) 
+                minIndex = j;
+            
+        }
+        
+        //swaping to get the desired position, sorting (-->)
+        swap(arr[minIndex], arr[i]);
+    }
+}
