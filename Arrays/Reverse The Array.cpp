bbbba0e2f09a958a825bd95a59b74345c57541d2
Subimal Das <er.subimaldas@gmail.com>
2022-07-19 14:38:48 +0530

    Reverse the array after M element
    https://www.codingninjas.com/codestudio/problems/reverse-the-array_1262298?leftPanelTab=0

--- Arrays/Reverse The Array.cpp
+++ Arrays/Reverse The Array.cpp
@@ -1,5 +1,7 @@
 void reverseArray(vector<int> &arr , int m)
 {
+    
+    //Here the given position is m and wee have to reverse element after M
     int start =m+1;
     int end = arr.size() - 1;
     while (start <= end)
