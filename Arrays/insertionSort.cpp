18b4c968d5fbb4f4802b57cb66d96dee090ae86a
Subimal Das <er.subimaldas@gmail.com>
2022-07-19 08:08:55 +0530

    Insertion Sort
    https://www.codingninjas.com/codestudio/problems/insertion-sort_3155179?leftPanelTab=0

--- Arrays/insertionSort.cpp
+++ Arrays/insertionSort.cpp
@@ -1,1 +1,31 @@
-#include
+Approach: its like Arranging the cards, in insertion sort we just shift the element not directly sort it.  
+
+  
+Time Complexity: O(n2)
+Space Complexity:O(1)
+
+CODE:
+
+void insertionSort(int n, vector<int> &arr){
+    
+       int i = 1;
+       while(i<n){
+         
+           int temp= arr[i];
+           int j =i-1;
+             while (j>=0){
+                  
+              //shift to the right  
+               if(arr[j]>temp){ 
+                 arr[j+1]=arr[j];
+                }
+               else{
+                    // stop
+                    break;
+                }
+                j--;
+            }
+           arr[j+1]= temp;
+           i++;
+    }
+}
