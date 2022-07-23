f0017f771160a7333615ce62f40174fe89ac4958
Subimal Das <er.subimaldas@gmail.com>
2022-07-23 13:37:43 +0530

    17. Sum of two Arrays.
    https://www.codingninjas.com/codestudio/problems/sum-of-two-arrays_893186

---
+++ Arrays/sum of two arrays.cpp
@@ -0,0 +1,67 @@
+Question: https://www.codingninjas.com/codestudio/problems/sum-of-two-arrays_893186
+
+
+Approach 1:
+Time Complexity = O(n+m)
+Space Complexity= O(n+m)
+
+// For reverse Function, because the value will be arranging from the last 
+vector<int>  reverse( vector<int> v){
+     int s=0;
+     int e= v.size()-1;
+     while(s<e){
+        swap(v[s++],v[e--]);
+    }
+    return v;
+}
+
+
+
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	int i=n-1;
+    int j= m-1;
+    int carry= 0;
+  vector<int> ans;
+    
+ while(i>=0 && j>=0){
+     int val1= a[i];
+     int val2= b[j];
+     int sum= val1 + val2 + carry;
+       
+     
+     carry= sum/10;
+     sum = sum%10;
+     ans.push_back(sum);
+     i--;
+     j--;
+ }
+    
+    //1st case where element is present in 1st array
+        while(i>=0){
+        int sum= a[i]+ carry; 
+        carry= sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+    }
+    
+      //2nd case where element is present in 2nd array
+        while(j>=0){
+        int sum= b[j] + carry; 
+        carry= sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        j--;
+    }
+     //For carry part
+      while(carry!=0){
+      int sum= carry;
+      carry= sum/10;
+      sum = sum%10;
+      ans.push_back(sum);
+   }
+    
+    return  reverse(ans);
+    
+    
+}
